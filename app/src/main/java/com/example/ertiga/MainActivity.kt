5fec277d420a2aa1c7aa9528f8f26d653fe31abf
JosephineTanA <98298035+JosephineTanA@users.noreply.github.com>
2023-03-02 14:21:02 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/ertiga/MainActivity.kt
+++ app/src/main/java/com/example/ertiga/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.title = ("Mode List")
+        supportActionBar?.title = ("Ertiga")
 
         rvSerets = findViewById(R.id.rv_seret)
         rvSerets?.setHasFixedSize(true)
